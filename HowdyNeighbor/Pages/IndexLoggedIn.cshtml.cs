e85e86bc0d517b85acb2a321107b157069d5db17
bet45140 <bet45140@yahoo.com>
2020-11-29 00:21:37 -0500

    Added login form on login page and made some pages for our demo

---
+++ HowdyNeighbor/Pages/IndexLoggedIn.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HowdyNeighbor.Pages
+{
+    public class IndexLoggedInModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPostSearchList(string searchString)
+        {
+            TempData["searchString"] = searchString;
+            TempData.Keep();
+            return RedirectToPage("/SearchList");
+        }
+    }
+
+}
+    
