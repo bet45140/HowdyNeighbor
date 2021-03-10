2a99a958757e65c70b87bade40f032b7f05a717f
Evan Batsch <evan@refractlabs.com>
2021-03-10 16:33:55 -0500

    feat: database setup

---
+++ HowdyNeighbor/Pages/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace HowdyNeighbor.Pages.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HowdyNeighborContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.ChecklistTask.Any())
+            {
+                return; // Database has been seeded
+            }
+
+            var tasks = new ChecklistTask[]
+            {
+                new ChecklistTask{Id=1, Action="Make sure that utilities are set up", IsComplete=false, Date=DateTime.Now},
+                new ChecklistTask{Id=2, Action = "Transfer perscriptions to new pharmacy", IsComplete=false, Date=DateTime.Now.AddDays(1)},
+                new ChecklistTask{Id=3, Action = "Get in contact with new neighbors", IsComplete=false, Date=DateTime.Now.AddDays(6)},
+                new ChecklistTask{Id=4, Action = "Purchase parks membership", IsComplete=false, Date=DateTime.Now.AddDays(14)},
+            };
+
+            context.ChecklistTask.AddRange(tasks);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ HowdyNeighbor/Pages/Data/HowdyNeighborContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HowdyNeighbor.Pages.Data
+{
+    public class HowdyNeighborContext : DbContext
+    {
+        public HowdyNeighborContext()
+        {
+        }
+
+        public HowdyNeighborContext (DbContextOptions<HowdyNeighborContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ChecklistTask> ChecklistTask { get; set; }
+    }
+}
